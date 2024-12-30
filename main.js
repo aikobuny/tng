7b3665750f3084b8b8340125b29b9417615f725f
aikobuny <aa.fakri143@gmail.com>
2024-12-30 17:34:42 +0800

    asd

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-function fc() {
-  let elem = document.getElementById('button')
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.msRequestFullscreen) {
-    elem.msRequestFullscreen();
-  } else if (elem.mozRequestFullScreen) {
-    elem.mozRequestFullScreen();
-  } else if (elem.webkitRequestFullscreen) {
-    elem.webkitRequestFullscreen();
-  }
-}
+var PRESETS;
 
-function fc2() {
-  document.body.webkitRequestFullScreen();
-}
+fetch('/presets.json').then(x => x.json()).then(data => {
+  let parent = document.getElementById('_preset');
+  PRESETS = data
+  PRESETS.forEach(e => {
+    let option = document.createElement('option');
+    option.innerHTML = e.name
+    option.setAttribute('value', e.name);
+    parent.appendChild(option)
+  });
+})
+var SELECT = document.getElementById('_preset')
+SELECT.addEventListener('change', function() {
+  let _receiver = document.getElementById('_receiver');
+  let _remark = document.getElementById('_remark');
+  let r = PRESETS.find(item => item.name === SELECT.value)
+  console.log(r)
+
+  _receiver.value = r.receiver;
+  _remark.value = r.remark ? _receiver.value: _receiver.value
+})
 
 function z(n) {
   return ('0' + n).slice(-2)
@@ -22,7 +28,7 @@ function z(n) {
 // Time
 function updateTime() {
   var d = new Date()
-  document.getElementsByClassName('time')[0].innerHTML = `<br>${z(d.getDay())}/${z(d.getMonth())}/${z(d.getFullYear())} ${z(d.getHours())}:${z(d.getMinutes())}:${z(d.getSeconds())}`;  
+  document.getElementsByClassName('time')[0].innerHTML = `${z(d.getDay())}/${z(d.getMonth())}/${z(d.getFullYear())} ${z(d.getHours())}:${z(d.getMinutes())}:${z(d.getSeconds())}`;  
 }
 
 function requestAmount() {
@@ -34,6 +40,28 @@ function requestAmount() {
   updateTime();
 }
 
-updateTime();
+function back() {
+  document.getElementsByClassName('panel')[0].style.display = 'block';
+  document.getElementsByClassName('receipt')[0].style.display = 'none';
+}
+
+function submit() {
+  let _amount = document.getElementById('_amount');
+  let _receiver = document.getElementById('_receiver');
+  let _remark = document.getElementById('_remark');
+  let _preset = document.getElementById('_preset');
+
+  if (_preset.value != "none") {
+
+  }
+
+  document.getElementById('amount').innerHTML = `RM <span>${Number(_amount.value).toFixed(2)}</span>`;
+  document.getElementsByClassName('receiver')[0].innerHTML = `${_receiver.value.replace(/ /g, "<br>")}`
+  document.getElementsByClassName('remark')[0].innerHTML = `${_remark.value ? _receiver.value: "Fund Transfer"}`
+
+  document.getElementsByClassName('panel')[0].style.display = 'none';
+  document.getElementsByClassName('receipt')[0].style.display = 'block';
+  updateTime();
+}
 
 window.navigator.standalone = true
